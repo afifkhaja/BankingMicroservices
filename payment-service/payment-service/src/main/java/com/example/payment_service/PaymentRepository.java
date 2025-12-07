74a5419fe539b90ebd328b7c6d03ed5bcd4e18c1
afifkhaja <70686793+afifkhaja@users.noreply.github.com>
2025-12-06 22:39:34 -0600

    Delete payment-service/payment-service directory

--- payment-service/payment-service/src/main/java/com/example/payment_service/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.payment_service;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-
-}
--- payment-service/payment-service/src/main/java/com/example/payment_service/PaymentStatus.java
+++
@@ -1,7 +0,0 @@
-package com.example.payment_service;
-
-public enum PaymentStatus {
-	PENDING,
-	SUCCESS,
-	FAILED
-}
